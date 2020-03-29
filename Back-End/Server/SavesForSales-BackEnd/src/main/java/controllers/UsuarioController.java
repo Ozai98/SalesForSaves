a0ee8ac4fd5eb080f717ec2224c8e0fe5b7bd572
gdguerrerog <gdguerrerog@unal.edu.co>
2020-03-29 18:10:47 -0500

    :sparkles::alembic: changed create user request to a post request

--- Back-End/Server/SavesForSales-BackEnd/src/main/java/controllers/UsuarioController.java
+++ Back-End/Server/SavesForSales-BackEnd/src/main/java/controllers/UsuarioController.java
@@ -9,7 +9,13 @@ import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +31,8 @@ import services.Services;
 @EnableAutoConfiguration
 public class UsuarioController {
 
+    public static final String ROUTE = "usuario";
+
     private Dao<Usuario, Integer> userDao;
 
     @PostConstruct
@@ -32,31 +40,29 @@ public class UsuarioController {
         userDao = DatabaseController.getInstance().userDao();
     }
 
-	@GetMapping("/crear")
-    public Response crear(@RequestParam(value = "nombre") String nombre, 
-                          @RequestParam(value = "email") String email,
-                          @RequestParam(value = "password") String password) 
+	@PostMapping(value = ROUTE + "/crear", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public Response crear(String nombre, String correo, String password)
     {
+
         // Verifing email
-        if(!Services.validateEmail(email)) return new Response(false, null, "bad email");
+        if(!Services.validateEmail(correo)) return new Response(false, null, "bad email");
+
 
         // Creating new user
-        Usuario nuevo = new Usuario();
-        nuevo.setNombre(nombre);
-        nuevo.setCorreo(email);
-        nuevo.setPassword(Services.cryptPassword(password));
+        Usuario newUser = new Usuario();
+        newUser.setNombre(nombre);
+        newUser.setCorreo(correo);
+        newUser.setPassword(Services.cryptPassword(password));
         try{
             // Saving new user
-            userDao.create(nuevo);
-            return new Response(true, nuevo, "user created");
+            userDao.create(newUser);
+            return new Response(true, newUser, "user created");
         }catch(SQLException ex){
             // Error saving
             Services.handleError(ex);
             return new Response(false, null, ex);
         }
-    }
-    
-    
+    }    
 }
 
 class Response{
