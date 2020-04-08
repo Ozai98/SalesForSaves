4aea1ff6f01d8ac6fc1ae53d4c395be553e37d48
draco1006 <mbarragani@unal.edu.co>
2020-04-08 16:07:59 -0500

    Medio controlador de productos

--- Back-End/Server/SavesForSales-BackEnd/src/main/java/com/example/demo/controllers/ProductoController.java
+++ Back-End/Server/SavesForSales-BackEnd/src/main/java/com/example/demo/controllers/ProductoController.java
@@ -15,11 +15,22 @@ import com.example.demo.database.models.Producto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import com.example.demo.database.DatabaseController;
 
 @RestController
 @EnableAutoConfiguration
 @RequestMapping("/producto")
 public class ProductoController{
+
+	private Dao<Producto, Integer> productoDao;
+
+    @PostConstruct
+    public void init() {
+        productoDao = DatabaseController.getInstance().productoDao();
+    }
+
+
+
 	@GetMapping("/{proveedor}")
 	public Producto[] getProductos(@PathVariable String proveedor){
             Producto product = new Producto();
@@ -28,9 +39,44 @@ public class ProductoController{
 	}
         
 	@GetMapping("/{id}/{proveedor}")
-	public Producto getProducto(@PathVariable String proveedor, @PathVariable Long id){
-            Producto product = new Producto();
-            product.setProveedor(proveedor);
-            return product;
+	public ProductResponse getProducto(@PathVariable String proveedor, @PathVariable Integer id){
+			Producto product;
+		try {
+			product = productoDao.queryForId(id);
+			return  ProductResponse.producto(true, product, "");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return  ProductResponse.producto(false, null, "");
+		}
+			
+			
+	}
+	public interface ProductResponse{
+		static ProductResponse producto(boolean ok, Producto p, String msg){
+			return new ProductoResp(ok, p, msg);
+		}
+		static ProductResponse productos(boolean o, Producto p[], String s){
+			return new ProductosResp(o, p, s);
+		}
+	}
+	public static class ProductoResp implements ProductResponse{
+		public final boolean ok;
+		public final Producto producto;
+		public final String msg;
+		public ProductoResp(boolean ok, Producto product, String msg){
+            this.ok = ok;
+            this.producto = product;
+			this.msg = msg;
+		}
+	}
+	public static class ProductosResp implements ProductResponse{
+		public final boolean ok;
+		public final Producto productos[];
+		public final String msg;
+		ProductosResp(boolean o, Producto p[], String s){
+			this.ok = o;
+			this.productos = p;
+			this.msg = s;
+		}
 	}
 }
