1bdd655a4e5ee911eaa0190b751eb1258f6c0007
daniel19066 <dpallaresr@unal.edu.co>
2020-04-10 16:42:54 -0500

    se agrega pagina inicial y componente de busqueda

---
+++ Front-End/src/components/search-bar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div clas="search-bar">
+        <input type="text" placeholder="Search.." name="search">
+        <button type="submit"><i class="fa fa-search"></i></button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- Front-End/src/views/Home.vue
+++ Front-End/src/views/Home.vue
@@ -1,7 +1,51 @@
 <template>
-  <div id="Home"></div>
+  <div class="home">
+    <div class="titulo">
+          <h1>SALES FOR SAVES</h1>
+    </div>
+
+
+        <searchbar class="search-bar"></searchbar>
+  </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import searchbar from '@/components/search-bar.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    searchbar
+  }
+}
 </script>
+
+<style>
+
+.titulo{
+  background-color: #A1FFCA;
+  display: flex;
+  font-family: Oswald;
+  font-size: 65px;
+}
+
+.search-bar input[type=text] {
+
+  padding: 6px;
+  border: none;
+  margin-top: 8px;
+  font-size: 17px;
+}
+.search-bar button {
+
+  padding: 6px 10px;
+  background: #ddd;
+  font-size: 17px;
+  border: none;
+  cursor: pointer;
+}
+</style>
