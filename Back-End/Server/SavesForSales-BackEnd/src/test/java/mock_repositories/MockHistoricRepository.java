f73f46a43e852f83d17e71eb9ccc8e4d4c8a238f
Ernesto <ebastidasp@unal.edu.co>
2020-05-18 14:38:07 -0500

    Test Historic

--- Back-End/Server/SavesForSales-BackEnd/src/test/java/mock_repositories/MockHistoricRepository.java
+++ Back-End/Server/SavesForSales-BackEnd/src/test/java/mock_repositories/MockHistoricRepository.java
@@ -11,19 +11,53 @@ package mock_repositories;
  */
 
 import com.example.demo.database.HistoricRepository;
-import com.example.demo.database.models.Client;
 import com.example.demo.database.models.Historic;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class MockHistoricRepository {
-    private HashMap<Integer, Client> mockTable;
+public class MockHistoricRepository implements HistoricRepository{
+    private HashMap<Integer, Historic> mockTable;
     private AtomicInteger idCounter ;
     
-    public MockClientRepository(){
+    public MockHistoricRepository(){
         mockTable = new HashMap();
         idCounter = new AtomicInteger();
+    }
+    
+    @Override
+    public List<Historic> getForUserAndState(Integer usrId, String state) throws Exception{
+        LinkedList<Historic> exit = new LinkedList();
+        for(Historic hsc: mockTable.values()) if(hsc.getUser().getId() == usrId && hsc.getState().compareTo(state) == 0) exit.add(hsc);
+        return exit;
+    }
+    
+    @Override
+    public List<Historic> getForProviderAndState(Integer usrId, String state) throws Exception{
+        LinkedList<Historic> exit = new LinkedList();
+        for(Historic hsc: mockTable.values()) if(hsc.getUser().getId() == usrId && hsc.getState().compareTo(state) == 0) exit.add(hsc);
+        return exit;
+    }
+    
+    @Override
+    public void create(Historic hsc) throws Exception {
+        hsc.setId(idCounter.getAndIncrement());       
+        mockTable.put(hsc.getId(), new Historic(hsc));
+    }
+
+    @Override
+    public Historic getById(int ID) throws Exception {
+        return mockTable.getOrDefault(ID, null);
+    }
+
+    @Override
+    public void update(Historic obj) throws Exception {
+        mockTable.put(obj.getId(), obj);
+    }
+
+    @Override
+    public void refresh(Historic obj) throws Exception {
+        
     }
 }
