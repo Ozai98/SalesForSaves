3c054a88a08e0caf8f2c01ab31551fc45b8cfaad
draco1006 <mbarragani@unal.edu.co>
2020-06-09 18:45:08 -0500

    compresor y desacompresot

--- Back-End/Server/SavesForSales-BackEnd/src/main/java/com/example/demo/services/Services.java
+++ Back-End/Server/SavesForSales-BackEnd/src/main/java/com/example/demo/services/Services.java
@@ -1,10 +1,13 @@
 
 package com.example.demo.services;
 
+import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.regex.Pattern;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 import org.apache.tomcat.util.codec.binary.Base64;
 
@@ -17,30 +20,63 @@ public class Services {
 	private static MessageDigest md;
 	private static Pattern emailPattern;
 
-	public static void startServices(){
-		try{
+	public static void startServices() {
+		try {
 			md = MessageDigest.getInstance("SHA-256");
-		} catch(NoSuchAlgorithmException ex){
+		} catch (NoSuchAlgorithmException ex) {
 			ex.printStackTrace();
 		}
 		emailPattern = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
-		
+
 	}
 
-	public static String cryptPassword(String password){
+	public static String cryptPassword(String password) {
 		byte[] crypt = md.digest(password.getBytes(StandardCharsets.UTF_8));
 		return new String(Base64.encodeBase64(crypt));
 	}
 
-	public static void handleError(Exception ex){
+	public static void handleError(Exception ex) {
 		ex.printStackTrace();
 	}
 
-	public static boolean validateEmail(String email){
-            
+	public static boolean validateEmail(String email) {
+
 		return emailPattern.matcher(email).matches();
 	}
 
-	
+	// compress the image bytes before storing it in the database
 
-} 
+	public static byte[] compressBytes(byte[] data) {
+		Deflater deflater = new Deflater();
+		deflater.setInput(data);
+		deflater.finish();
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+		byte[] buffer = new byte[1024];
+		while (!deflater.finished()) {
+			int count = deflater.deflate(buffer);
+			outputStream.write(buffer, 0, count);
+		}
+		try {
+			outputStream.close();
+		} catch (Exception e) {}
+		System.out.println("Compressed Image Byte Size - " + outputStream.toByteArray().length);
+		return outputStream.toByteArray();
+	}
+
+	// uncompress the image bytes before returning it to the angular application
+
+	public static byte[] decompressBytes(byte[] data) {
+		Inflater inflater = new Inflater();
+		inflater.setInput(data);
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+		byte[] buffer = new byte[1024];
+		try {
+			while (!inflater.finished()) {
+				int count = inflater.inflate(buffer);
+				outputStream.write(buffer, 0, count);
+			}
+			outputStream.close();
+		} catch (Exception ioe) {}
+		return outputStream.toByteArray();
+	}
+}
