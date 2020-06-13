a7b6c5e02a24a0f9d1f2e9574618d90b1624089a
abihido <jcristanchor@unal.edu.co>
2020-06-12 19:18:13 -0500

    cambios en el mapa

--- Front-End/src/components/maps/configMap.js
+++ Front-End/src/components/maps/configMap.js
@@ -1,12 +1,12 @@
 const configMapa = {
-    clickableIcons: false,
-    streetViewControl: false,
+    clickableIcons: true,
+    streetViewControl: true,
     panControlOptions: false,
     gestureHandling: "cooperative",
     mapTypeControl: false,
     zoom: 7,
     minZoom: 3,
-    maxZoom: 9
+    maxZoom: 16
   };
    
   export { configMapa};
