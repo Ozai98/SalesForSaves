0f78ee372b2b683f85ffa30ee36393e6bc19d132
draco1006 <mbarragani@unal.edu.co>
2020-06-17 10:58:04 -0500

    comment envia user

--- Back-End/Server/SavesForSales-BackEnd/src/main/java/com/example/demo/controllers/CommentController.java
+++ Back-End/Server/SavesForSales-BackEnd/src/main/java/com/example/demo/controllers/CommentController.java
@@ -37,7 +37,14 @@ public class CommentController {
 	}
 
 	private Comments normalize(Comments comment) {
-		comment.setProvider(UserController.normalize(comment.getProvider()));
+		try {
+			userRepository.refresh(comment.getUser());
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		comment.setProvider(ProviderController.normalize(comment.getProvider()));
 		comment.setUser(UserController.normalize(comment.getUser()));
 		return comment;
 	}
@@ -58,11 +65,10 @@ public class CommentController {
 	}
 
 	@GetMapping("/search/{id}")
-	public Response<Comments[]> searchByProvider(@PathVariable int id) {
+	public Response<Comments[]> searchByProvider(@PathVariable Integer id) {
 		List<Comments> comments;
 		try {
 			comments = commentRepository.searchByProvider(id);
-			System.out.println(comments.size());
 			Comments[] respoComments = new Comments[comments.size()];
 			int i = 0;
 			for(Comments comments2: comments) respoComments[i++] = normalize(comments2);
