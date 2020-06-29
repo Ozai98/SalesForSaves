cc8dafb078e5eecafd52f0f1b74b6e37ac571d3a
Andres <rasenagan1198@gmail.com>
2020-06-29 15:48:20 -0500

    Overall look of msg given

--- Front-End/src/views/messages.vue
+++ Front-End/src/views/messages.vue
@@ -1,54 +1,63 @@
 <template>
-    <div id=all>
-        <div id=tittle>
-                QUE ONDAAAAAAA
+  <div id="all">
+    <div id="msgCont">
+      <div id="sideBar">
+        <div id="tittle">
+          Chats
         </div>
         <div id="contacts">
-                <div id="SubContacts" v-for="i in dataH" :key="i.id">
-                    <img class="imagen" :src="getImage(i.image)" @click="getProv(i.provider)">
-                </div>
+          <div id="SubContacts" v-for="i in dataH" :key="i.id">
+            <img
+              class="imagen"
+              :src="getImage(i.image)"
+              @click="getProv(i.provider)"
+            />
+          </div>
         </div>
-        <div id=chat>
-            <div id="msg" v-for="j in msg" :key="j.idU.mail" >
-                <div class="contenedor" v-if="j.idU.id==other">
-                     <img id="imagen2" :src="getImage(j.idProv.avatar)" >
-                     <p id="user">{{j.idProv.name}}</p>
-                     <p id="hora">ayer</p>
-                     <label id="mensaje">{{j.idm}}</label>  
-                </div>
-                <div class="contenedor" v-if="j.idU.id==id">
-                     <img class="imagen3" :src="getImage(j.idU.avatar)" >
-                     <p class="user2">{{j.idU.name}}</p>
-                     <p id="hora2">ayer</p>
-                     <label id="mensaje2">{{j.idm}}</label> 
-                </div>
-                
+      </div>
+
+      <div id="chat">
+        <div id="chatBubble">
+          <div id="msg" v-for="j in msg" :key="j.idU.mail">
+            <div class="contenedor" v-if="j.idU.id == other">
+              <img id="imagen2" :src="getImage(j.idProv.avatar)" />
+              <p id="user">{{ j.idProv.name }}</p>
+              <p id="hora">ayer</p>
+              <label id="mensaje">{{ j.idm }}</label>
             </div>
-            <div class="contenedor" >
-                     <img class="imagen3" src="../assets/imgs/user.svg" >
-                     <p class="user2">USER</p>
-                     <input id="entrada" v-model="m" v-on:keyup.enter="setMsg()"/> 
+            <div class="contenedor" v-if="j.idU.id == id">
+              <img class="imagen3" :src="getImage(j.idU.avatar)" />
+              <p class="user2">{{ j.idU.name }}</p>
+              <p id="hora2">ayer</p>
+              <label id="mensaje2">{{ j.idm }}</label>
             </div>
+          </div>
         </div>
+
+        <div class="inputBar">
+          <input id="entrada" v-model="m" v-on:keyup.enter="setMsg()" />
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import request from "@/services/request.service.js";
 export default {
-    data(){
-        return{
-            dataH:Object,
-            msg:Object,
-            id: this.$store.getters.returnUser.id,
-            other:-1,
-            m:''
-        }
-    },
-    methods:{
-        Backbb() {
+  data() {
+    return {
+      dataH: Object,
+      msg: Object,
+      id: this.$store.getters.returnUser.id,
+      other: -1,
+      m: "",
+    };
+  },
+  methods: {
+    Backbb() {
       this.dataH = [];
-      
+
       request.getHistoricbyId(this.id, (data) => {
         if (data.ok) {
           for (const hist of data.classX) {
@@ -64,148 +73,184 @@ export default {
         }
       });
     },
-    getmsg(){
-        this.msg=[]
-        if(this.other!=-1){
-            console.log(this.id+' '+this.other+' obtene')
-            request.getMsg(this.id,this.other,(data)=>{
-            if (data.ok){
-                console.log("si entro")
-                for( const M of data.classX){
-                    this.msg.push(
-                        {
-                            idU: M.user,
-                            idP: M.provider,
-                            idm: M.content
-                        }
-                        )
-                    }
-                }
-            });
-            
-        }
-        console.log(this.msg)
-    },
-    setMsg(){
-        if(this.other!=-1){
-            console.log(this.id+' '+this.other+' '+this.m)
-        request.setMsg(this.id,this.other,this.m, (data)=>{
-            if(data.ok){
-                this.getmsg();
-                this.$forceUpdate();
-                this.m='';
+    getmsg() {
+      this.msg = [];
+      if (this.other != -1) {
+        console.log(this.id + " " + this.other + " obtene");
+        request.getMsg(this.id, this.other, (data) => {
+          if (data.ok) {
+            console.log("si entro");
+            for (const M of data.classX) {
+              this.msg.push({
+                idU: M.user,
+                idP: M.provider,
+                idm: M.content,
+              });
             }
+          }
         });
-        }
+      }
+      console.log(this.msg);
     },
-    getProv(provider){
-        this.other=provider;
-        this.getmsg();
-        this.$forceUpdate();
+    setMsg() {
+      if (this.other != -1) {
+        console.log(this.id + " " + this.other + " " + this.m);
+        request.setMsg(this.id, this.other, this.m, (data) => {
+          if (data.ok) {
+            this.getmsg();
+            this.$forceUpdate();
+            this.m = "";
+          }
+        });
+      }
+    },
+    getProv(provider) {
+      this.other = provider;
+      this.getmsg();
+      this.$forceUpdate();
     },
     getImage(image) {
-      return 'data:image/jpeg;base64,' + image;
-    }
+      return "data:image/jpeg;base64," + image;
     },
-    mounted(){
-        this.Backbb();
-    }
-    
-}
+  },
+  mounted() {
+    this.Backbb();
+  },
+};
 </script>
 
 <style scoped>
-#all{
-    background-color: white;
-    font-family: "Oswald", sans-serif;
-    font-weight: bold;
+#all {
+  background-color: white;
+  font-family: "Oswald", sans-serif;
+  font-weight: bold;
+  min-height: 90%;
+  display: block;
 }
-#tittle{
-    font-size: 5vw;
+#msgCont {
+  display: inline-block;
+  width: 80%;
+  height: 90vh;
+  background-color: #a1ffca;
+  margin: 2% 9% 0 9%;
+  border: 1px solid #ff8e43;
+  border-radius: 1vw 1vw 0 0;
+  border-bottom: none;
 }
-.imagen{
-    width: 7vw;
-    height:7vw;
-    border-radius: 3vw;
-    margin: 2vw;
-    border: 0.4vw solid #a1ffca;
+#sideBar {
+  float: left;
+  height: 100%;
+  width: 9%;
 }
-#contacts{
-    display: flex;
-    align-items: flex-start;
-    margin: 2vw;
-    overflow:scroll;
-    overflow-y: hidden;
+#chat {
+  float: right;
+  height: 100%;
+  width: 90%;
 }
-.contenedor{
-    display: grid;
-    margin: 1vw;
-    border: 0.2vw solid gray;
-    border-radius: 3vw;
+#chatBubble {
+  background-color: white;
+  border: 1px solid #ff8e43;
+  border-radius: 1vw;
+  width: 96%;
+  height: 80%;
+  margin: 1vw 2vw 1vw 1vw;
 }
-#imagen2{
-    grid-row: 2;
-    grid-column: 1;
-    width: 4vw;
-    height: 4vw;
-    justify-self: center;
-    margin: 0 0 1vw;
+
+#tittle {
+  font-size: 2vw;
 }
-#user{
-    grid-row: 1;
-    grid-column: 1;
-    margin: 0 0 1vw;
+.imagen {
+  width: 4vw;
+  height: 4vw;
+  border-radius: 3vw;
+  border: 0.4vw solid #a1ffca;
 }
-#hora{
-    grid-row: 1;
-    grid-column: 3;
+#contacts {
+  margin: 1vw;
+  overflow: auto;
 }
-#mensaje{
-    grid-row: 2;
-    grid-column: 2/4;
-    text-align: left;
-    background-color: gray;
-    border-radius: 2vw;
-    text-indent: 2vw ;
-    margin: 1vw;
-    font-weight: lighter;
-    font-size: 3vw;
-    vertical-align: center;
+.contenedor {
+  height: 2vw;
+  display: grid;
+  margin: 1vw;
+  border: 1px solid #ff8e43;
+  border-radius: 3vw;
 }
-.imagen3{
-    grid-row: 2;
-    grid-column: 3;
-    width: 4vw;
-    height: 4vw;
-    justify-self: center;
-    margin: 0 0 1vw;
+.inputBar {
+  width: 96%;
+  height: 2vw;
+  display: grid;
+  margin: 1vw;
+  border: 1px solid #ff8e43;
+  border-radius: 3vw;
+  background-color: white;
 }
-.user2{
-    grid-row: 1;
-    grid-column: 3;
-    margin: 0 0 1vw;
+#imagen2 {
+  grid-row: 2;
+  grid-column: 1;
+  width: 4vw;
+  height: 4vw;
+  justify-self: center;
+  margin: 0 0 1vw;
 }
-#hora2{
-    grid-row: 1;
-    grid-column: 1;
+#user {
+  grid-row: 1;
+  grid-column: 1;
+  margin: 0 0 1vw;
 }
-#mensaje2{
-    grid-row: 2;
-    grid-column: 1/3;
-    text-align: left;
-    background-color:blue;
-    border-radius: 2vw;
-    text-indent: 2vw ;
-    margin: 1vw;
-    font-weight: lighter;
-    font-size: 3vw;
-    vertical-align: center;
-    color: white;
+#hora {
+  grid-row: 1;
+  grid-column: 3;
 }
-#entrada{
-    grid-row: 1/3;
-    grid-column: 1/3;
-    margin: 1vw;
-    border-radius: 4vw;
+#mensaje {
+  grid-row: 2;
+  grid-column: 2/4;
+  text-align: left;
+  background-color: gray;
+  border-radius: 2vw;
+  text-indent: 2vw;
+  margin: 1vw;
+  font-weight: lighter;
+  font-size: 3vw;
+  vertical-align: center;
+}
+.imagen3 {
+  grid-row: 2;
+  grid-column: 3;
+  width: 4vw;
+  height: 4vw;
+  justify-self: center;
+  margin: 0 0 1vw;
+}
+.user2 {
+  grid-row: 1;
+  grid-column: 3;
+  margin: 0 0 1vw;
+}
+#hora2 {
+  grid-row: 1;
+  grid-column: 1;
+}
+#mensaje2 {
+  grid-row: 2;
+  grid-column: 1/3;
+  text-align: left;
+  background-color: blue;
+  border-radius: 2vw;
+  text-indent: 2vw;
+  margin: 1vw;
+  font-weight: lighter;
+  font-size: 3vw;
+  vertical-align: center;
+  color: white;
+}
+#entrada {
+  font-family: "Verdana", sans-serif;
+  color: #888;
+  padding-left: 1vw;
+  border-radius: 4vw;
+  height: 2vw;
+  width: 70%;
+  border: none;
 }
 </style>
